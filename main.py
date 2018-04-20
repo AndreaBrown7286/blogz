f629888a89d9aab293213ec024365b5230f8e9ab
Andrea Brown <andreabrown7286@gmail.com>
2018-04-19 20:07:43 -0500

    added User class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:mypassword@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:mypassword@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -12,10 +12,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=True)
     body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, author):
         self.title = title
         self.body = body
+        self.author = author
+
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    blogs = db.relationship('Blog', backref='author')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     
 @app.route('/blog', methods=['GET','POST'])
@@ -57,7 +70,7 @@ def newblog():
             return render_template('newblog.html', blog_body_error=blog_body_error,
                 blog_title_error=blog_title_error)
         
-        newblog = Blog(blog_title, blog_body)
+        newblog = Blog(blog_title, blog_body, author)
         db.session.add(newblog)
         db.session.commit()
         newblog_id = str(newblog.id)
